6f1b79e3c1866cd73d6f952781daebeb38a52059
Konstantin Polyakov <shyzik93@mail.ru>
2019-01-05 14:51:08 +0000

    Module is fixed

--- api_amocrm.py
+++ api_amocrm.py
@@ -1,6 +1,6 @@
 import requests
 import json
-import config_parser
+import configparser
 import os
 
 class Base():
@@ -10,32 +10,35 @@ class Base():
         self.login = login
         self.client_hash = client_hash
 
-        self.domain = 'http://'+subdomain+'amocrm.ru/'
+        self.domain = 'http://'+subdomain+'.amocrm.ru/'
         self.url = self.domain+'api/v2/'
 
         self.s = requests.Session()
 
         self.headers = {
-            'user-agent': 'amoCRM-API-client/1.0',
+            'User-Agent': 'amoCRM-API-client/1.0',
             'Content-Type': 'application/json',
         }
 
     def auth(self):
         url = self.domain + 'private/api/auth.php?type=json'
         data = {'USER_LOGN':self.login, 'USER_HASH': self.client_hash}
-        res = self.s.post(url)
+        res = self.s.post(url, data=json.dumps(data), headers=self.headers)
 
         if res.status_code in [200, 204]:
-            res_json = res.json['response']
+            res_json = res.json()['response']
             if  res_json['auth']:
                 print('authed')
             else:
                 print('not authed')
+        elif res.status_code == 401:
+            res_json = res.json()['response']
+            print('auth failed:', res_json['error_code'], res_json['error'])
         else:
-            print('server error. Status code:', res.status_code)
+            print('server error. Status code:', res.status_code, res.content)
 
     def request(self, name, data):
-        data = json.dump(data)
+        data = json.dumps(data)
 
         res = self.s.post(self.url+name, data=data, headers=self.headers)
         if res.status_code == 200:
@@ -43,7 +46,9 @@ class Base():
 
 if __name__ == '__main__':
     config = configparser.ConfigParser()
-    config = config.read(os.path.join(os.path.dirname(__file__), 'parser_config.ini'))
+    #config.read(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'parser_config.ini'))
+    config.read('../parser_config.ini')
+    #print(dict(config['DEFAULT']))
     config = config['amocrm']
 
     amo = Base(config['subdomain'], config['login'], config['client_hash'])
