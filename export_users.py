be31245e854363dff0d2a8a448eb3c5f99858bba
Konstantin Polyakov <shyzik93@mail.ru>
2019-01-05 08:15:16 +0000

    exporting is more full

--- export_users.py
+++ export_users.py
@@ -1,6 +1,7 @@
 import config_site_avito as cs
 import csv
 import os
+import datetime
 
 if __name__ == "__main__":
 
@@ -12,15 +13,34 @@ if __name__ == "__main__":
     f = open(output_path, 'w')
     csvw = csv.writer(f)
 
-    sql = "SELECT * FROM user, user_contact WHERE user.user_id=user_contact.user_contact_user_id"
+    csvw.writerow([
+        'full name (contact)',
+        'position (contact)',
+        'responsible',
+        'who create contact',
+        'mobile phone',
+        'when create contact',
+        'url of the profile',
+    ])
+
+    when_create_contact = datetime.date.today().strftime('%d.%m.%Y')
+
+    sql = "SELECT * FROM user, user_contact, user_type  WHERE user.user_id=user_contact.user_contact_user_id AND user.user_type_id=user_type.user_type_id LIMIT 10"
     rows = c.execute(sql).fetchall()
     for row in rows:
+
+        user_position = ''
+        responsible = ''
+        who_create_contact = 'Polyakov Konstantin'
+
         csvw.writerow([
             row['user_name'],
+            user_position,
+            responsible,
+            who_create_contact,
             row['user_contact_data'],
-            'Polyakov Konstantin',
-            cur_date,
-            row['user_name'],
+            when_create_contact,
+            row['user_url'],
         ])
 
     f.close()
